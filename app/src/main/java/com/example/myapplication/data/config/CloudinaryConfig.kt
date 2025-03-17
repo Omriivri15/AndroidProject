4858559a5e815b9c95e2d467dc523f72de61ffac
Omriivri15 <omriivri15@gmail.com>
2025-03-18 00:32:36 +0200

    Update CloudinaryConfig.kt

--- app/src/main/java/com/example/myapplication/data/config/CloudinaryConfig.kt
+++ app/src/main/java/com/example/myapplication/data/config/CloudinaryConfig.kt
@@ -2,6 +2,4 @@ package com.example.myapplication.data.config
 
 object CloudinaryConfig {
     const val CLOUD_NAME = "dxzdqld9a"
-    const val API_KEY = "957949912889967"
-    const val API_SECRET = "DlSm7BuM1nL4FK-60t1VsSFdCyI"
 }
