1e65f8bb52d6024a48b31646a6529f8fdc51cb13
roiyraz88 <roiyraz88@gmail.com>
2025-03-29 12:45:40 +0300

    fixes

--- app/src/main/java/com/example/myapplication/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/ProfileFragment.kt
@@ -23,7 +23,6 @@ import com.google.firebase.auth.FirebaseAuth
 
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
 import com.google.firebase.storage.FirebaseStorage
 import com.squareup.picasso.Picasso
